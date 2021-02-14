05bdd6a022a1ece846f5d00d4fa9250fe62e0c8a
Ashutosh Mohapatra <63866874+bpn123@users.noreply.github.com>
2021-02-14 22:09:22 +0530

    uploaded successfully
    python program to find all positive numbers in a range of numbers.

---
+++ positiv no.py
@@ -0,0 +1,13 @@
+# Python program to print positive Numbers in a List 
+  
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+for no in list1:     
+    if no >= 0: 
+       print(no, end = " ")
+
+print()
+       
+for no in list2:
+    if no >= 0:
+        print(no, end = " ")
